c137014e5ada86ba0ca84595d4829aeb9f378c72
nelliott82 <nikkoelliott@gmail.com>
2023-08-12 18:06:22 -0400

    Updated camel case for folder references per production requirements

--- client/src/components/cards/Admin.jsx
+++ client/src/components/cards/Admin.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import LoginForm from './adminCards/LoginForm.jsx';
-import Navbar from './adminCards/Navbar.jsx';
-import CreateInvite from './adminCards/CreateInvite.jsx';
-import Invitations from './adminCards/Invitations.jsx';
-import Responded from './adminCards/Responded.jsx';
+import LoginForm from './admincards/LoginForm.jsx';
+import Navbar from './admincards/Navbar.jsx';
+import CreateInvite from './admincards/CreateInvite.jsx';
+import Invitations from './admincards/Invitations.jsx';
+import Responded from './admincards/Responded.jsx';
 import axios from 'axios';
 
 const Admin = ({ display, login, setLogin, cardDiv, buttons }) => {
